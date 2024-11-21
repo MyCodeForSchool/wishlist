49e5820525204e43cd593b8c14e4948920f92e8f
Your Name <you@example.com>
2024-11-20 23:47:00 -0600

    Part 2 of Travel video 1 and 30 minutes of video 2.

--- travel_wishlist/forms.py
+++ travel_wishlist/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.forms import DateInput
+
 from .models import Place
 
 #This ties together the form object with the model object so that django knows how they relate
@@ -6,3 +8,15 @@ class NewPlaceForm(forms.ModelForm):
     class Meta:
         model = Place
         fields = ('name', 'visited')
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class TripReviewForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = ('notes', 'date_visited', 'photo')
+        widgets = {
+            'date_visited': DateInput() #this widget ensure conformity with date formatting
+        }
+
--- travel_wishlist/models.py
+++ travel_wishlist/models.py
@@ -1,11 +1,19 @@
 #This code is used to define the data models/tables that will be used for the database
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here.  After changing original models, you will need to run migrations.
 class Place(models.Model):
+    user = models.ForeignKey('auth.User', null=False, on_delete=models.CASCADE)#Cascade deletes associated data
     name = models.CharField(max_length=200)#constraint set of max 200 characters
     visited = models.BooleanField(default=False)
+    notes = models.TextField(blank=True, null = True)
+    date_visited = models.DateTimeField(blank=True, null=True)
+    photo = models.ImageField(upload_to='user_images/', blank=True, null=True)
+
 
     def __str__(self):
-        return f'{self.name} visited? {self.visited}'
+        photo_str = self.photo.url if self.photo else 'no photo'
+        notes_str = self.notes [100:] if self.notes else 'no notes'
+        return f'{self.name} visited? {self.visited} on {self.date_visited}.  Photo {photo_str}. Notes {notes_str}.'
 
